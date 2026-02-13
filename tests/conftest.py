cadd223a6310d27ab17950b7f7cbe45bc55f8ff8
RaresPopa22 <poparares1992@gmail.com>
2026-02-13 23:22:02 +0200

    add unit test for MissingValueHandler

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,53 @@ def sample_validate_data():
 def sample_validate_columns(sample_validate_data):
     return list(sample_validate_data.columns.values)
     
+@pytest.fixture
+def sample_categorical_data():
+    np.random.seed(1)
+    n_samples = 5
+
+    return pd.DataFrame({
+        'Pool QC': np.random.choice(['Fa', 'TA', 'Gd', 'Ex'], n_samples),
+        'Fence': np.random.choice(['MnWw', 'GdWo', 'MnPrv', 'GdPrv'], n_samples),
+        'Misc Feature': [np.nan] * n_samples
+    })
+
+@pytest.fixture
+def sample_numerical_data():
+    np.random.seed(1)
+    n_samples = 5
+
+    return pd.DataFrame({
+        'Mas Vnr Area': np.random.randint(0, 4987, n_samples),
+        'Bsmt Full Bath': np.random.randint(0, 4987, n_samples),
+        'Bsmt Half Bath': [np.nan] * n_samples
+    })
+
+@pytest.fixture
+def sample_cat_and_num_data(sample_categorical_data, sample_numerical_data):
+    categorical_cols = sample_categorical_data.columns.to_list()
+    numerical_cols = sample_numerical_data.columns.to_list()
+    df = pd.concat([sample_categorical_data, sample_numerical_data], axis=1)
+    config = {'categorical': categorical_cols, 'numerical': numerical_cols}
+
+    return df, config
+
+@pytest.fixture
+def sample_clean_cat_and_num_data():
+    n_samples = 2
+
+    cat = pd.DataFrame({
+        'Pool QC': np.random.choice(['Fa', 'TA', 'Gd', 'Ex'], n_samples),
+        'Fence': np.random.choice(['MnWw', 'GdWo', 'MnPrv', 'GdPrv'], n_samples),
+    })
+    num = pd.DataFrame({
+        'Mas Vnr Area': np.random.randint(0, 4987, n_samples),
+        'Bsmt Full Bath': np.random.randint(0, 4987, n_samples),
+    })
+
+    categorical_cols = cat.columns.to_list()
+    numerical_cols = num.columns.to_list()
+    df = pd.concat([cat, num], axis=1)
+    config = {'categorical': categorical_cols, 'numerical': numerical_cols}
+
+    return df, config
---
+++ tests/pipeline/test_missing_value_handler.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import pytest
+from src.pipeline.missing_value_handler import MissingValueHandler
+
+
+class TestMissingValueHandler:
+
+    def test_happy_path(self, sample_cat_and_num_data):
+        df, config = sample_cat_and_num_data
+        handler = MissingValueHandler(config)
+        res = handler.transform(df)
+
+        assert not res.isna().any().any()
+        assert (res['Misc Feature'] == 'None').all()
+        assert (res['Bsmt Half Bath'] == 0).all()
+
+    def test_unsupported_feature_type(self, sample_cat_and_num_data):
+        df, config = sample_cat_and_num_data
+        config = config.copy()
+        config['imaginary'] = ['cat']
+
+        with pytest.raises(ValueError, match=r"(?i)(?=.*unsupported)(?=.*imaginary)"):
+            handler = MissingValueHandler(config)
+            res = handler.transform(df)
+
+    def test_missing_mandatory_type(self, sample_cat_and_num_data):
+        df, config = sample_cat_and_num_data
+        config = config.copy()
+        config.pop('categorical')
+        with pytest.raises(TypeError):
+            handler = MissingValueHandler(config)
+            res = handler.transform(df)
+
+    def test_passthrough(self, sample_clean_cat_and_num_data):
+        df, config = sample_clean_cat_and_num_data
+
+        handler = MissingValueHandler(config)
+        res = handler.transform(df)
+        pd.testing.assert_frame_equal(df, res)
+
+
